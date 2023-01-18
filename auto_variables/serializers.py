1f543527d6c5347c4c0a76bebb631eb7d782fffa
Dicle Acet <dicleacet@outlook.com>
2023-01-18 16:36:07 +0300

    update Serializer

--- auto_variables/serializers.py
+++ auto_variables/serializers.py
@@ -1,16 +1,61 @@
 from rest_framework import serializers
-from django.utils.translation import gettext_lazy as _
 from auto_variables.models import ExcelFile
+from django.core.files import File
+from requests import get
 from auto_variables.utils import AutoVariable
-from auto_variables.tasks import upload
+
+
+class VariableInfoSerializer(serializers.Serializer):
+    name = serializers.CharField(allow_null=False, required=True)
+    categorical = serializers.BooleanField(allow_null=False, required=True)
+    open_ended = serializers.BooleanField(allow_null=False, required=True)
+    numeric = serializers.BooleanField(allow_null=False, required=True)
+
+    def create(self, validated_data):
+        return VariableInfoSerializer(**validated_data)
+
+    def update(self, instance, validated_data):
+        return VariableInfoSerializer(**validated_data)
 
 
 class VariableFileSerializer(serializers.ModelSerializer):
+    data = VariableInfoSerializer(many=True, read_only=True)
+
     class Meta:
         model = ExcelFile
-        fields = ('id', 'api_document_id', 'file_url', 'file', 'output_file', 'is_download', 'is_finished')
+        fields = (
+            'id', 'api_document_id', 'file_url', 'file', 'output_file', 'is_download', 'is_finished', 'data',
+        )
         read_only_fields = ('id', 'file', 'output_file', 'is_download', 'is_finished')
 
     def create(self, validated_data):
         self.instance = ExcelFile.objects.create(**validated_data)
+        # upload.delay(self.instance.id, self.instance.file_url)
+        response = get(self.instance.file_url)
+        with open(self.instance.file_url.split('/')[-1], 'wb') as file:
+            file.write(response.content)
+        with open(self.instance.file_url.split('/')[-1], 'rb') as file:
+            output_file = File(file, name='output.xlsx')
+            self.instance.file = output_file
+            self.instance.is_download = True
+            self.instance.save()
+
+        infos = AutoVariable(self.instance.file).info_file()
+        info_list = []
+        for key, alias in infos.items():
+            if alias:
+                info_list.append({
+                    'name': key,
+                    'categorical': True,
+                    'open_ended': False
+                })
+            else:
+                info_list.append({
+                    'name': key,
+                    'categorical': False,
+                    'open_ended': True
+                })
+
+        self.instance.data = info_list
+        self.instance.save()
         return self.instance
--- auto_variables/tasks.py
+++ auto_variables/tasks.py
@@ -2,18 +2,18 @@ from time import sleep
 from celery import shared_task
 from auto_variables.models import ExcelFile
 from django.core.files import File
-from django.core.files.storage import FileSystemStorage
-from pathlib import Path
+from requests import get
 
 
 @shared_task
-def upload(id, file_url):
+def upload(excel_id, file_url):
     print('Downloading...')
     sleep(10)
-    storage = FileSystemStorage()
-    path_object = Path(file_url)
-    with path_object.open(mode='rb') as file:
-        output_file = File(file, name=path_object.name)
-        instance = ExcelFile(id=id, output_file=output_file, is_download=True)
+    response = get(file_url)
+    with open(file_url.split('/')[-1], 'wb') as file:
+        file.write(response.content)
+    with open('output.xlsx', 'rb') as file:
+        output_file = File(file, name='output.xlsx')
+        instance = ExcelFile.objects.get(id=excel_id, file=output_file, is_download=True)
         instance.save()
-    print('Uploaded!')
+    print('Downloaded')
